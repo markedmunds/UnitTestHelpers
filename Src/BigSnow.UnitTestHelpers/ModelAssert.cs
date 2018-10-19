451034efbe284f57927b15aa46dcb5e430a38b35
Mark Edmunds <mark.edmunds@gmail.com>
2018-10-19 09:11:54 -0600

    Initial commit

---
+++ Src/BigSnow.UnitTestHelpers/ModelAssert.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace BigSnow.UnitTestHelpers
+{
+    public class ModelAssert
+    {
+        public static void AreEqual<T>(T expected, T actual) 
+        {
+            AreEqual<T>(expected, actual, false, null);
+        }
+
+        public static void AreEqual<T>(T expected, T actual, bool ignoreIds) 
+        {
+            AreEqual<T>(expected, actual, ignoreIds, null);
+        }
+
+        public static void AreEqual<T>(T expected, T actual, string[] excludePropertyFilter) 
+        {
+            AreEqual<T>(expected, actual, false, excludePropertyFilter);
+        }
+
+        public static void AreEqual<T>(T expected, T actual, bool ignoreIds, string[] excludePropertyFilter) 
+        {
+            PropertyInfo[] properties = expected.GetType().GetProperties();
+            foreach (PropertyInfo prop in properties)
+            {
+                //only validate system and primative types only
+                if (!prop.PropertyType.Namespace.StartsWith("System"))
+                    continue;
+
+                //Ignore Ids
+                if (ignoreIds && prop.Name.EndsWith("id", StringComparison.CurrentCultureIgnoreCase))
+                    continue;
+
+                //Ignore properties not in the propertyFilter
+                if (excludePropertyFilter != null && excludePropertyFilter.Contains(prop.Name))
+                    continue;
+
+                //TODO: we don't want to recurse into arrays or enumerables, only check the counts
+                object propValue = prop.GetValue(expected, null);
+                if (propValue is IEnumerable || prop.PropertyType.IsArray || (prop.PropertyType.IsGenericType
+                    && prop.PropertyType.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>))))
+                {
+                    continue;
+                }
+
+                Assert.AreEqual(propValue, prop.GetValue(actual, null), expected.GetType().Name + "." + prop.Name);
+            }
+        }
+    }
+}
---
+++ Src/BigSnow.UnitTestHelpers/RandomValueGenerator.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BigSnow.UnitTestHelpers
+{
+	public sealed class RandomValueGenerator
+	{
+		#region Thread-safe, lazy Singleton
+		/// <summary>
+		/// This is a thread-safe, lazy singleton.  See http://www.yoda.arachsys.com/csharp/singleton.html
+		/// for more details about its implementation.
+		/// </summary>
+		public static RandomValueGenerator Instance
+		{
+			get
+			{
+				return Nested.RandomValueGenerator;
+			}
+		}
+
+		/// <summary>
+		/// Private constructor to enforce singleton
+		/// </summary>
+		private RandomValueGenerator() { }
+
+		/// <summary>
+		/// Assists with ensuring thread-safe, lazy singleton
+		/// </summary>
+		private class Nested
+		{
+			static Nested() { }
+			internal static readonly RandomValueGenerator RandomValueGenerator = new RandomValueGenerator();
+		}
+		#endregion
+
+		private readonly Random _rand = new Random();
+		public int GetRandomInteger()
+		{
+			return _rand.Next();
+		}
+
+		public int GetRandomInteger(int min, int max)
+		{
+			return _rand.Next(min, max);
+		}
+
+		public long GetRandomLong()
+		{
+			return Convert.ToInt64((long.MaxValue * 1.0 - 0 * 1.0) * _rand.NextDouble() + 0 * 1.0);
+		}
+
+		public double GetRandomDouble()
+		{
+			return _rand.NextDouble();
+		}
+
+		public string GetRandomString(int size)
+		{
+			return GetRandomString(size, false);
+		}
+
+		public string GetRandomString(int size, bool lowerCase)
+		{
+			StringBuilder builder = new StringBuilder();
+			char ch;
+			for (int i = 0; i < size; i++)
+			{
+				ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * _rand.NextDouble() + 65)));
+				builder.Append(ch);
+			}
+			if (lowerCase)
+				return builder.ToString().ToLower();
+			return builder.ToString();
+		}
+
+		public bool GetRandomBoolean()
+		{
+			return Convert.ToBoolean(_rand.Next(0, 2));
+		}
+	}
+}
